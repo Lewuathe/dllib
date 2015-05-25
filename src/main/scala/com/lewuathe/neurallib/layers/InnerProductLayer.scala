12ee46e095df3a80ff28f5be7423b336d3f0b21b
lewuathe <lewuathe@me.com>
2015-05-25 22:53:39 +0900

    Implement forward and backward

--- src/main/scala/com/lewuathe/neurallib/layers/InnerProductLayer.scala
+++ src/main/scala/com/lewuathe/neurallib/layers/InnerProductLayer.scala
@@ -1,16 +1,31 @@
 package com.lewuathe.neurallib.layers
 
-import com.lewuathe.neurallib.Datum
+import breeze.linalg._
+import com.lewuathe.neurallib.{Channel, Datum}
+
 
 /**
  * Created by lewuathe on 5/23/15.
  */
 class InnerProductLayer extends Layer {
+  var channels: Int = _
+  var inputDim: Int = _
+  var outputDim: Int = _
   /**
    * Initialize parameters of this layer
    */
   override def init(conf: Map[String, String]): Unit = {
-    ;
+    channels = conf("channels").toInt
+    inputDim = conf("input").toInt
+    outputDim = conf("output").toInt
+
+    val paramChannels = for (i <- 0 until channels) yield {
+      new Channel(Vector.rand(inputDim * outputDim) - 0.5, outputDim, inputDim)
+    }
+
+    // The height of parameter matrix is output dimension.
+    // The width of parameter matrix is input dimension.
+    param = Datum(paramChannels, channels, outputDim, inputDim)
   }
 
   /**
@@ -19,7 +34,18 @@ class InnerProductLayer extends Layer {
    * @param downstream
    * @return output Datum
    */
-  override def forward(downstream: Datum): Datum = ???
+  override def forward(downstream: Datum): Datum = {
+    require(downstream.channels == channels)
+    val retChannels = for ((dsc, pc)
+                              <- downstream.getData zip param.getData) yield {
+      val paramMatrix
+        = pc.getValues.toDenseVector.toDenseMatrix.reshape(outputDim, inputDim)
+      require(dsc.getValues.length == inputDim)
+      val ret = paramMatrix * dsc.getValues
+      new Channel(ret, outputDim, 1)
+    }
+    Datum(retChannels, channels, outputDim, 1)
+  }
 
 
   /**
@@ -30,7 +56,17 @@ class InnerProductLayer extends Layer {
    * because of diff calculation.
    * @param upstream
    */
-  override def backward(upstream: Datum): _root_.com.lewuathe.neurallib.Datum = ???
+  override def backward(upstream: Datum): _root_.com.lewuathe.neurallib.Datum = {
+    require(upstream.channels == channels)
+    val retChannels = for ((usc, pc) <- upstream.getData zip param.getData) yield {
+      val paramMatrix
+        = pc.getValues.toDenseVector.toDenseMatrix.reshape(outputDim, inputDim)
+      require(usc.getValues.length == outputDim)
+      val ret = paramMatrix.t * usc.getValues
+      new Channel(ret, inputDim, 1)
+    }
+    Datum(retChannels, channels, inputDim, 1)
+  }
 
   /**
    * Calculate error update difference on each input error
