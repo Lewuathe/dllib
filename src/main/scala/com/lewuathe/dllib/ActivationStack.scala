8399030df8cec427cbd68dd321a52073875ab9f8
lewuathe <lewuathe@me.com>
2016-01-02 00:15:43 +0900

    Description of ActivationStack

--- src/main/scala/com/lewuathe/dllib/ActivationStack.scala
+++ src/main/scala/com/lewuathe/dllib/ActivationStack.scala
@@ -4,5 +4,11 @@ import scala.collection.mutable.Stack
 
 import breeze.linalg.Vector
 
+/**
+  * ActivationStack collects all raw output and activations of each layer.
+  * This is a stack listing (u, z)
+  * u: raw output that multiplication weight and input
+  * z: f(u) assuming f is an activation function
+  */
 class ActivationStack extends Stack[(Vector[Double], Vector[Double])] {}
 
